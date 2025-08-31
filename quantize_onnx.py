6a9c3a623bbb9dca028e2428de5dfc647a9064ce
晓龙 周 <zxlweiruan@outlook.com>
2025-08-31 22:31:28 +0800

    添加量化脚本。

---
+++ quantize_onnx.py
@@ -0,0 +1,18 @@
+import onnx
+from onnxruntime.quantization import quantize_dynamic, QuantType
+
+
+def quantize_onnx_model(input_model_path, output_model_path):
+    quantize_dynamic(
+        input_model_path,
+        output_model_path,
+        weight_type=QuantType.QInt8,  # 量化权重为Int8
+
+    )
+    print(f"Quantized model saved to {output_model_path}")
+
+
+if __name__ == "__main__":
+    input_onnx_model = "model_FP16.onnx"
+    output_onnx_model = "model_int8.onnx"
+    quantize_onnx_model(input_onnx_model, output_onnx_model)
